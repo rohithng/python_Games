e3bb90db46ca1bb7282d39367aec6a227cd70da7
rohithng <89979397+rohithng@users.noreply.github.com>
2021-10-20 08:56:45 -0700

    Add files via upload

---
+++ roll_the_dices.py
@@ -0,0 +1,70 @@
+import random import tkinter as tk min = 1 max = 6
+
+roll_again = 1
+
+while roll_again == 1:
+
+def write_slogan():
+    print("Tkinter is easy to use!")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, text="QUIT", fg="red",command=quit)
+             
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,text="rolling the dice....",command=write_slogan)
+               
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
+def write_slogan():
+    print("Tkinter is easy to use!")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, text="QUIT", fg="red",command=quit)
+             
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,text="the values are.....",command=write_slogan)
+               
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
+def write_slogan():
+    print("Tkinter is easy to use!")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, text="QUIT", fg="red",command=quit)
+             
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,text=random.randint(min, max),command=write_slogan)
+               
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
+def write_slogan():
+    print("Tkinter is easy to use!")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, text="QUIT", fg="red",command=quit)
+             
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,text=random.randint(min, max),command=write_slogan)
+               
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
+
+
+
+roll_again = int(input("Roll the dices again?"))
